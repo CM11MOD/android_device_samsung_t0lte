12277955560d78d803c30b8a3a1a708ad3714fb5
burstlam <burstlam@gmail.com>
2014-05-02 02:12:03 +0800

    Use Google gcc4.7 to build kernel

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -37,6 +37,9 @@ else
 TARGET_KERNEL_CONFIG := slim_t0lte_defconfig
 endif
 
+TARGET_GCC_VERSION_AND := LINARO
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/t0lte/rootdir/fstab.smdk4x12
 RECOVERY_FSTAB_VERSION := 2
